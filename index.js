29132b49100244004ee9a65542207c69f0223950
TONNYCHWEYA <chweyatonny@gmail.com>
2022-06-10 09:33:06 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+
+
+function superbowlWin(array){
+  
+
+   function findW(item){
+  
+     return item.result === "W"
+   }
+
+   const winningYear =array.find(findW)
+   if(winningYear == undefined){
+     return undefined;
+   }
+   
+   return winningYear.year;
+   
+}
